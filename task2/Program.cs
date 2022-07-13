bccdbc40ed50b9a5817b50a692371ecfb500c3e2
Anastasiia Bedareva <beda2049@gmail.com>
2022-07-13 16:32:47 +0300

    task2 comleted

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите трехзначное число.");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + NumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
